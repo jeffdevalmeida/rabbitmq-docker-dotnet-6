90de16c87d77c5d7620cbbdd5940433b6b19b968
Jefferson Almeida <jefferson.almeida@konia.com.br>
2022-03-15 16:58:18 -0300

    Add project files.

---
+++ RabbitMQ.Objects/Order.cs
@@ -0,0 +1,12 @@
+﻿namespace RabbitMQ.Objects
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public string ProductName { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public int ProductId { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ RabbitMQ.Sample/Controllers/OrderController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Objects;
+using System.Text;
+
+namespace RabbitMQ.Sample.Controllers
+{
+    [Route("api/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(202, Type = typeof(Order))]
+        public IActionResult Create([FromBody] Order order)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "order-queue", false, false, false, null);
+
+                string message = JsonConvert.SerializeObject(order);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "", routingKey: "order-queue", basicProperties: null, body: body);
+            }
+
+            return Accepted(order);
+        }
+    }
+}
